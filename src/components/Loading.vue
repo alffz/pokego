8f8ef53bd712827258b5855461c677f510d4d149
alffz <alfriansyah0408@gmail.com>
2023-11-18 23:14:06 +0700

    create loading modal

---
+++ src/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="modal fade show" id="loading" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" style="display: block; padding-left: 0px;" aria-modal="true" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body bg-warning rounded">
+        <p >Loading ...</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
