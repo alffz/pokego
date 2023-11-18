5d9cb2efb6dd65761f991f26d1d65d79250debe0
alffz <alfriansyah0408@gmail.com>
2023-11-18 23:12:33 +0700

    create list pokemon componen

--- src/components/ListPokemon.vue
+++ src/components/ListPokemon.vue
@@ -1,13 +1,13 @@
 <template>
-  <div v-for="index of card"  class="col-sm-6 col-md-4 col-lg-3 ">
+  <div v-for="{name,images} in pokemon.pokemons"  class="col-sm-6 col-md-4 col-lg-3 ">
     <div class="card mb-3" >
       <div class="row g-0">
-        <div  @click="showDetail()" class="col-md-4 pointer" data-bs-toggle="modal" data-bs-target="#detailPokemon">
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" class="img-fluid rounded-start" alt="...">
+        <div  @click="showDetail(name,images)" class="col-md-4 pointer" data-bs-toggle="modal" data-bs-target="#detailPokemon">
+          <img :src="images" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8 ">
           <div class="card-body d-flex justify-content-between">
-            <h5 class="card-title">Kodok Ijo {{ index }}</h5>
+            <h5 class="card-title"> {{ name }}</h5>
             <h5 class="card-title"><i class="bi bi-heart-fill text-danger"></i></h5>
           </div>
         </div>
@@ -17,10 +17,53 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+// import {onBeforeUnmount, onMounted, ref} from 'vue'
+import { showAlert } from '../utils/utils.js';
+import axios from 'axios';
 
-// const detailPokemon = ref(false)
-const card = ref(20)
+import { usePokemon } from '../stores/pokemon.js';
+
+const pokemon = usePokemon()
+
+const showDetail = async (name,image)=>{
+  try{
+    const data = await axios.get(`pokemon/${name}`)
+    if(data.status === 200){
+
+      const name = data.data.name
+      const ability = data.data.abilities[0].ability.name
+      const species = data.data.species.name
+      const type = data.data.types[0].type.name
+      const hp = data.data.stats[0].base_stat
+      const attack = data.data.stats[1].base_stat
+      const defense = data.data.stats[2].base_stat
+      const special_attack = data.data.stats[3].base_stat
+      const special_defense = data.data.stats[4].base_stat
+      const speed = data.data.stats[5].base_stat
+
+      const detail = {
+        image,
+        name,
+        ability,
+        species,
+        type,
+        hp,
+        attack,
+        defense,
+        special_attack,
+        special_defense,
+        speed
+      }
+      console.log(detail)
+      pokemon.detailPokemon = detail
+    }else{
+      showAlert({message:"someting went wrong",background:'alert-warning'})
+    }
+  }catch(err){
+    showAlert({message:"unexpected error",background:'alert-danger'})
+  }
+}
+
 </script>
 
 <style scoped>
